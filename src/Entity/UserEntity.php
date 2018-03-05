f52e7623c7691140677b55810436a03d431341d4
Thiago Paes <mrprompt@gmail.com>
2018-03-04 22:07:38 -0300

    feature(user module): init user module

---
+++ src/Entity/UserEntity.php
@@ -0,0 +1,153 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Entity;
+
+use DateTime;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+use Symfony\Bridge\Doctrine\Validator\Constraints\UniqueEntity;
+
+/**
+ * User Entity
+ *
+ * @ORM\Table(name="users")
+ * @ORM\Entity(repositoryClass="App\Repository\UserRepository")
+ * @ORM\HasLifecycleCallbacks
+ * @UniqueEntity(
+ *      fields={"email"},
+ *      message="An user with this email is already registered"
+ * )
+ */
+class UserEntity
+{
+    use Base;
+    
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     * @Assert\NotBlank(message="Name should not be blank")
+     */
+    private $name;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="email", type="string", length=255, unique=true)
+     * @Assert\Email(message="Email is invalid")
+     */
+    private $email;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="password", type="string", length=255)
+     * @Assert\NotBlank(message="Password should not be blank")
+     */
+    private $password;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="created_at", type="datetime")
+     * @Assert\Type("\DateTime")
+     */
+    private $createdAt;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="updated_at", type="datetime")
+     * @Assert\Type("\DateTime")
+     */
+    private $updatedAt;
+
+    /**
+     * constructor
+     */
+    public function __construct()
+    {
+        $this->createdAt = new DateTime;
+        $this->updatedAt = new DateTime;
+    }
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set email
+     *
+     * @param string $email
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    /**
+     * Get email
+     *
+     * @return string
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set password
+     *
+     * @param string $password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Get password
+     *
+     * @return string
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+}
---
+++ tests/Entity/UserEntityTest.php
@@ -0,0 +1,199 @@
+<?php
+declare(strict_types = 1);
+
+namespace App\Tests\Entity;
+
+use App\Tests\Entity\Traits\ChangeProtectedAttribute;
+use App\Entity\UserEntity;
+use PHPUnit\Framework\TestCase;
+use stdClass;
+use DateTime;
+
+/**
+ * User test case.
+ *
+ * @author Thiago Paes <mrprompt@gmail.com>
+ */
+class UserEntityTest extends TestCase
+{
+    use ChangeProtectedAttribute;
+
+    /**
+     * The user entity
+     * 
+     * @var UserEntity
+     */
+    private $user;
+
+    /**
+     * Setup
+     */
+    public function setUp()
+    {
+        parent::setUp();
+        
+        $this->user = new UserEntity;
+    }
+
+    /**
+     * Shutdown
+     */
+    public function tearDown()
+    {
+        $this->user = null;
+        
+        parent::tearDown();
+    }
+
+    /**
+     * @return multitype:multitype:number
+     */
+    public function validObjects()
+    {
+        $obj = new stdClass();
+        $obj->id = 1;
+        $obj->name  = 'Teste';
+        $obj->email = 'teste@teste.net';
+        $obj->password = 'fooo';
+        $obj->createdAt = new DateTime;
+        $obj->updatedAt = new DateTime;
+
+        return [
+            [
+                $obj
+            ]
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::getId
+     */
+    public function getIdReturnValue($obj)
+    {
+        $this->modifyAttribute($this->user, 'id', $obj->id);
+
+        $this->assertEquals($this->user->getId(), $obj->id);
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::setName
+     */
+    public function setNameReturnEmpty($obj)
+    {
+        $result = $this->user->setName($obj->name);
+
+        $this->assertEmpty($result);
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::getName
+     */
+    public function getNameReturnValue($obj)
+    {
+        $this->modifyAttribute($this->user, 'name', $obj->name);
+
+        $this->assertEquals($this->user->getName(), $obj->name);
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::setEmail
+     */
+    public function setEmailReturnEmpty($obj)
+    {
+        $result = $this->user->setEmail($obj->email);
+
+        $this->assertEmpty($result);
+    }
+    
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::getEmail
+     */
+    public function getEmailReturnValue($obj)
+    {
+        $this->modifyAttribute($this->user, 'email', $obj->email);
+
+        $this->assertEquals($this->user->getEmail(), $obj->email);
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::setPassword
+     */
+    public function setPasswordReturnEmpty($obj)
+    {
+        $result = $this->user->setPassword($obj->password);
+
+        $this->assertEmpty($result);
+    }
+    
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::getPassword
+     */
+    public function getPasswordReturnValue($obj)
+    {
+        $this->modifyAttribute($this->user, 'password', $obj->password);
+
+        $this->assertEquals($this->user->getPassword(), $obj->password);
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::setCreatedAt
+     */
+    public function setCreatedAtReturnEmpty($obj)
+    {
+        $result = $this->user->setCreatedAt($obj->createdAt);
+
+        $this->assertEmpty($result);
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::getCreatedAt
+     */
+    public function getCreatedAtReturnValue($obj)
+    {
+        $this->modifyAttribute($this->user, 'createdAt', $obj->createdAt);
+
+        $this->assertEquals($this->user->getCreatedAt(), $obj->createdAt);
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::setUpdatedAt
+     */
+    public function setUpdatedAtReturnEmpty($obj)
+    {
+        $result = $this->user->setUpdatedAt($obj->updatedAt);
+
+        $this->assertEmpty($result);
+    }
+
+    /**
+     * @test
+     * @dataProvider validObjects
+     * @covers       \App\Entity\UserEntity::getUpdatedAt
+     */
+    public function getUpdatedAtReturnValue($obj)
+    {
+        $this->modifyAttribute($this->user, 'updatedAt', $obj->updatedAt);
+
+        $this->assertEquals($this->user->getUpdatedAt(), $obj->updatedAt);
+    }
+}
